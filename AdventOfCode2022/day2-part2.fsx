9c60dde7167a7218eb28b89c6f07df5601ab363c
Rik Steenkamp <c5fstn@ewps.nl>
2022-12-08 22:50:54 +0100

    Advent day 2

---
+++ AdventOfCode2022/day2-part2.fsx
@@ -0,0 +1,3 @@
+let f = function | 'X' -> 0 | 'Y' -> 3 | _ -> 6
+let g = function | ("B X" | "A Y" | "C Z") -> 1 | ("C X" | "B Y" | "A Z") -> 2 | _ -> 3
+System.IO.File.ReadAllLines "2" |> (fun x -> Seq.sumBy g x + Seq.sumBy (Seq.last >> f) x)
