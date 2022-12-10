d9a9263d3a0c9a7f161d07726867731319775f02
Rik Steenkamp <c5fstn@ewps.nl>
2022-12-10 18:52:25 +0100

    Advent day 4

---
+++ AdventOfCode2022/day4-part1.fsx
@@ -0,0 +1,2 @@
+let f [| a; b; c; d |] = a >= c && b <= d || c >= a && d <= b
+System.IO.File.ReadAllLines "4" |> Seq.filter (fun s -> s.Replace(',', '-').Split('-') |> Array.map int |> f) |> Seq.length
