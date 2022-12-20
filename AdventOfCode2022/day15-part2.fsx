5f05db144a6dc842f07e51021a8c245bfb9edcbc
Rik Steenkamp <c5fstn@ewps.nl>
2022-12-20 07:54:09 +0100

    New day15-part2.fsx solution

--- AdventOfCode2022/day15-part2.fsx
+++ AdventOfCode2022/day15-part2.fsx
@@ -1,7 +1,6 @@
-let f (x,y) (i,j) = if i <= y + 1L && j >= x - 1L then Some ((i,j), (min x i, max y j)) else None
-let rec h bs c = match Seq.choose (f c) bs |> Seq.toList with | [] -> Set.add c bs | (b, d)::_ -> h (Set.remove b bs) d
-let w, cs, p = 4_000_000L, [|',';':';'=';' '|], Seq.fold h Set.empty
-let us = System.IO.File.ReadAllLines("15") |> Seq.map (fun s -> let xs = s.Split(cs) in [for i in [3;6;13;16] -> int64 xs[i]])
-let g m = us |> Seq.map (fun [x;y;i;j] -> let n = abs (x-i) + abs (y-j) - abs (y-m) in (x-n, x+n)) |> Seq.filter (fun (x, y) -> y >= x)
-          |> p |> Seq.tryPick (fun (x, y) -> if x > 0 && x < w+2L then Some (x-1L) else if y > -2 && y < w then Some (y+1L) else None)
-[0L..w] |> Seq.pick (fun m -> g m |> Option.map (fun x -> x * 4_000_000L + m))
+let w, cs, q, p = 4_000_000, [|',';':';'=';' '|], List.sortBy (fun (x, _, _) -> x), fun i (x, y, _) -> if x > i then i else max i (y + 1)
+let r = Seq.map (fun [x;y;i;j] -> let n = abs (x-i) + abs (y-j) - abs y in (x-n, x+n, y)) >> Seq.toList >> q
+let us = System.IO.File.ReadAllLines("15") |> Seq.map (fun s -> let xs = s.Split(cs) in [for i in [3;6;13;16] -> int xs[i]]) |> r
+let f m (x, y, z) = if m >= z then (x+1, y-1, z) else (x-1, y+1, z)
+let g, h = (fun bs m -> List.map (f m) bs |> q), fun (y, x) -> int64 x * 4_000_000L + int64 y
+[0..w] |> Seq.scan g us |> Seq.map (List.fold p 0) |> Seq.indexed |> Seq.find (fun (_, x) -> x <= w) |> h
